b008a4bcdcce75a23c7b55c95f6b0ab059986297
Popescu Andrei <andreigabrielpopescu556@gmail.com>
2021-11-12 23:04:20 +0200

    Resolved edge detection with doule threshold hysteresis

--- src/edge_detection.py
+++ src/edge_detection.py
@@ -22,13 +22,82 @@ def SobelFilter(image: np.ndarray, direction: str) -> np.ndarray:
         return res
 
 
+# Normalize the pixel array, so that values are <= 1
+def Normalize(image: np.ndarray) -> np.ndarray:
+    # img = np.multiply(img, 255 / np.max(img))
+    image = image / np.max(image)
+    return image
+
+
+# Double threshold Hysteresis Note that I have used a very slow iterative approach for ease of understanding,
+# a faster implementation using recursion can be done instead This recursive approach would recurse through every
+# strong edge and find all connected weak edges
+
+def DoThreshHyst(image: np.ndarray) -> np.ndarray:
+    high_threshold_ratio = 0.2
+    low_threshold_ratio = 0.15
+
+    g_sup = np.copy(image)
+
+    h = int(g_sup.shape[0])
+    w = int(g_sup.shape[1])
+
+    high_threshold = np.max(g_sup) * high_threshold_ratio
+    low_threshold = high_threshold * low_threshold_ratio
+
+    x = 0.1
+    old_x = 0
+
+    # The while loop is used so that the loop will keep executing till the number of strong edges do not change,
+    # i.e all weak edges connected to strong edges have been found
+    while old_x != x:
+        old_x = x
+        for i in range(1, h - 1):
+            for j in range(1, w - 1):
+                if g_sup[i, j] > high_threshold:
+                    g_sup[i, j] = 1
+                elif g_sup[i, j] < low_threshold:
+                    g_sup[i, j] = 0
+                else:
+                    if ((g_sup[i - 1, j - 1] > high_threshold) or
+                            (g_sup[i - 1, j] > high_threshold) or
+                            (g_sup[i - 1, j + 1] > high_threshold) or
+                            (g_sup[i, j - 1] > high_threshold) or
+                            (g_sup[i, j + 1] > high_threshold) or
+                            (g_sup[i + 1, j - 1] > high_threshold) or
+                            (g_sup[i + 1, j] > high_threshold) or
+                            (g_sup[i + 1, j + 1] > high_threshold)):
+                        g_sup[i, j] = 1
+
+        x = np.sum(g_sup == 1)
+
+    # This is done to remove/clean all the weak edges which are not connected to strong
+    # edges
+    g_sup = (g_sup == 1) * g_sup
+
+    return g_sup
+
+
 if __name__ == '__main__':
     # Read image
     img = plt.imread("../data/test.jpg")
     plt.imshow(img, cmap=plt.get_cmap('gray'))
     plt.show()
+
+    # Convert to grayscale
     img = rgb2gray(img)
     _img = SobelFilter(image=img, direction='x')
     plt.imshow(_img)
     plt.show()
+
+    # Normalize
+    _img = Normalize(_img)
+    plt.imshow(_img)
+    plt.show()
+
+    # Double threshold hysteresis
+    _img = DoThreshHyst(_img)
+    plt.imshow(_img)
+    plt.show()
+    
     plt.imsave("../data/test_sobel.jpg", _img)
