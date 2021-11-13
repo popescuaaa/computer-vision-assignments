c29f5d204d962baf71a435b28c4572c7c3ba3436
Popescu Andrei <andreigabrielpopescu556@gmail.com>
2021-11-13 14:40:30 +0200

    Introduced the method of pixel variability with custom masks to determine the location of objects in photos

--- src/blue_pool.py
+++ src/blue_pool.py
@@ -2,10 +2,22 @@ from edge_detection import Normalize, DoThreshHyst, SobelFilter
 import matplotlib.pyplot as plt
 from helpers import rgb2gray
 import numpy as np
-from scipy import ndimage
+
+
+def create_blue_pool_filter_010(image: np.ndarray) -> np.ndarray:
+    # Custom image with a single block of white pixels for pool
+    f = np.copy(image)
+    w, h = f.shape
+    for i in range(w):
+        for j in range(h):
+            if 180 < i < 200 and 232 < j < 248:
+                f[i, j] = 1
+            else:
+                continue
+    return f
+
 
 if __name__ == '__main__':
-
     # Read image
     img = plt.imread('../data/Gura_Portitei_Scara_010.jpg')
     # Convert to grayscale
@@ -18,15 +30,44 @@ if __name__ == '__main__':
     plt.imshow(_img)
     plt.show()
 
-    # Custom image with a single block of white pixels for pool
-    f = np.copy(_img)
-    w, h = f.shape
-    print(f.shape)
-    for i in range(w):
-        for j in range(h):
-            if 180 < i < 200 and 232 < j < 248:
-                f[i, j] = 1
-            else:
-                f[i, j] = 0
+    # Create blue pool filter
+    flt = create_blue_pool_filter_010(image=_img)
 
+    samples = [flt, _img]
+    sample_images = np.concatenate([image.reshape((1, image.shape[0], image.shape[1])) for image in samples], axis=0)
 
+    print(sample_images.shape)
+    plt.figure(1)
+    for i in range(sample_images.shape[0]):
+        plt.subplot(2, 2, i + 1)
+        plt.imshow(sample_images[i, ...])
+        plt.axis("off")
+    plt.subplots_adjust(0, 0, 1, 1, 0, 0)
+    plt.show()
+
+    # determine per-pixel variability, std() over all images
+    variability = sample_images.std(axis=0)
+    print(variability.shape)
+    plt.imshow(variability, interpolation="nearest", origin="lower")
+    plt.show()
+
+    plt.figure(2)
+    # determine bounding box
+    thresholds = [1.00]
+    colors = ["r", "b", "g"]
+    for threshold, color in zip(thresholds, colors):  # variability.mean()
+        non_empty_columns = np.where(variability.min(axis=0) < threshold)[0]
+        non_empty_rows = np.where(variability.min(axis=1) < threshold)[0]
+        boundingBox = (min(non_empty_rows), max(non_empty_rows), min(non_empty_columns), max(non_empty_columns))
+
+        # plot and print boundingBox
+        bb = boundingBox
+        plt.plot([bb[2], bb[3], bb[3], bb[2], bb[2]],
+                 [bb[0], bb[0], bb[1], bb[1], bb[0]],
+                 "%s-" % color,
+                 label="threshold %s" % threshold)
+
+    plt.xlim(0, variability.shape[1])
+    plt.ylim(variability.shape[0], 0)
+    plt.legend()
+    plt.show()
