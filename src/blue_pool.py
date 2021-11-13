5809c05d725e90999a72b55bfda45d260886d575
Popescu Andrei <andreigabrielpopescu556@gmail.com>
2021-11-13 16:49:55 +0200

    Resolved pixel variability for pool but not with original image

--- src/blue_pool.py
+++ src/blue_pool.py
@@ -7,36 +7,37 @@ import numpy as np
 def create_blue_pool_filter_010(image: np.ndarray) -> np.ndarray:
     # Custom image with a single block of white pixels for pool
     f = np.copy(image)
-    w, h = f.shape
+    w, h, _ = f.shape
     for i in range(w):
         for j in range(h):
+
             if 180 < i < 200 and 232 < j < 248:
-                f[i, j] = 1
-            else:
-                continue
+                for k in range(3):
+                    f[i, j, k] = 1
+                else:
+                    continue
     return f
 
 
 if __name__ == '__main__':
     # Read image
     img = plt.imread('../data/Gura_Portitei_Scara_010.jpg')
-    # Convert to grayscale
-    img = rgb2gray(img)
-    _img = SobelFilter(image=img, direction='x')
-    # Normalize
-    _img = Normalize(_img)
-    # Double threshold hysteresis
-    _img = DoThreshHyst(_img)
-    plt.imshow(_img)
-    plt.show()
+    # # Convert to grayscale
+    # img = rgb2gray(img)
+    # _img = SobelFilter(image=img, direction='x')
+    # # Normalize
+    # _img = Normalize(_img)
+    # # Double threshold hysteresis
+    # _img = DoThreshHyst(_img)
+    # plt.imshow(_img)
+    # plt.show()
 
     # Create blue pool filter
-    flt = create_blue_pool_filter_010(image=_img)
+    flt = create_blue_pool_filter_010(image=img)
 
-    samples = [flt, _img]
-    sample_images = np.concatenate([image.reshape((1, image.shape[0], image.shape[1])) for image in samples], axis=0)
+    samples = [flt, img]
+    sample_images = np.concatenate([image.reshape((1, image.shape[0], image.shape[1], image.shape[2])) for image in samples], axis=0)
 
-    print(sample_images.shape)
     plt.figure(1)
     for i in range(sample_images.shape[0]):
         plt.subplot(2, 2, i + 1)
@@ -53,7 +54,7 @@ if __name__ == '__main__':
 
     plt.figure(2)
     # determine bounding box
-    thresholds = [1.00]
+    thresholds = [5, 10, 20]
     colors = ["r", "b", "g"]
     for threshold, color in zip(thresholds, colors):  # variability.mean()
         non_empty_columns = np.where(variability.min(axis=0) < threshold)[0]
